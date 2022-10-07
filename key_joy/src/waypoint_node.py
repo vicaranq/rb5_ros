36c455f8cb6d07db522bcaf9bbafb28eabaa1362
vicaranq <vdarango@hotmail.com>
2022-10-06 23:46:00 -0700

    Update waypoint_node.py

--- key_joy/src/waypoint_node.py
+++ key_joy/src/waypoint_node.py
@@ -2,14 +2,41 @@
 import sys
 import rospy
 from sensor_msgs.msg import Joy
-from key_parser import get_key, save_terminal_settings, restore_terminal_settings
+from key_parser import  save_terminal_settings, restore_terminal_settings
 
-class KeyJoyNode:
+class WaypointNode:
     def __init__(self):
         self.pub_joy = rospy.Publisher("/joy", Joy, queue_size=1)
         self.settings = save_terminal_settings()
 
 
+    def run(self):
+
+        self.move_front(1) #move 1m?
+        
+        self.stop()
+
+    def move_front(self, x):
+        '''
+        Args:
+        x -> int type represeting meters
+        '''
+        # calibrate 
+
+        joy_msg = Joy()
+        joy_msg.axes = [0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0]
+        joy_msg.buttons = [0, 0, 0, 0, 0, 0, 0, 0]
+        if x >= 0: #key == 'w':
+            joy_msg.axes[1] = x
+        else:
+            joy_msg.axes[1] = -x
+        
+        
+        self.pub_joy.publish(joy_msg)
+
+
+        return x
+    '''
     def run(self):
         while True:
             # parse keyboard control
@@ -24,7 +51,7 @@ class KeyJoyNode:
             self.pub_joy.publish(joy_msg)
     
         self.stop()
-
+    
     def key_to_joy(self, key):
         flag = True
         joy_msg = Joy()
@@ -45,6 +72,7 @@ class KeyJoyNode:
         elif (len(key) > 0 and ord(key) == 27) or (key == '\x03'):
             flag = False
         return joy_msg, flag
+    '''
     
 
     def stop(self):
@@ -52,7 +80,7 @@ class KeyJoyNode:
 
 
 if __name__ == "__main__":
-    key_joy_node = KeyJoyNode()
-    rospy.init_node("key_joy")
-    key_joy_node.run()
+    waypoint_node = WaypointNode()
+    rospy.init_node("waypoint")
+    waypoint_node.run()
     
