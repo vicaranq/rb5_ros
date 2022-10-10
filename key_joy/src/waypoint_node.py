8c8cd564141fb21e587b6ebae57312238bdd66be
vicaranq <vdarango@hotmail.com>
2022-10-10 10:53:43 -0700

    Update waypoint_node.py

--- key_joy/src/waypoint_node.py
+++ key_joy/src/waypoint_node.py
@@ -37,7 +37,8 @@ class WaypointNode:
         joy_msg.axes = [0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0]
         joy_msg.buttons = [0, 0, 0, 0, 0, 0, 0, 0]
 
-        target_time = 2   # [seconds to get to a meter]
+        # target_time = 2   # [seconds to get to a meter]
+        target_time = 1.94  
         # ideal: target_time = x / (speed [m/s])
 
         t_start = time.time()
