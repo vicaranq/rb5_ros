12cb5f0af658e4c81b6ce93f59ee585b303fbd9f
vicaranq <vdarango@hotmail.com>
2022-10-11 08:54:03 -0700

    Update mpi_control.py

--- rb5_control/src/mpi_control.py
+++ rb5_control/src/mpi_control.py
@@ -65,7 +65,8 @@ class MegaPiController:
         # self.setFourMotors(-speed*0.8, speed*0.8, -speed*0.8, speed*0.9) # need more power on the left
         # self.setFourMotors(-speed*0.8, speed*0.8, -speed*0.85, speed*0.9) # need more left
         # self.setFourMotors(-speed*0.8, speed*0.8, -speed*1, speed*0.9) # need more on the right or less on left
-        self.setFourMotors(-speed*0.8, speed*0.8, -speed*0.9, speed*0.9)
+        # self.setFourMotors(-speed*0.8, speed*0.8, -speed*0.9, speed*0.9) # need more on left 
+        self.setFourMotors(-speed*0.8, speed*0.8, -speed*0.95, speed*0.9)  
 
 
     def carRotate(self, speed):
