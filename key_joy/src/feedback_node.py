7447e20f7408594a31f855098cb88ce67a1e5753
vicaranq <vdarango@hotmail.com>
2022-10-24 17:39:30 -0700

    Update feedback_node.py

--- key_joy/src/feedback_node.py
+++ key_joy/src/feedback_node.py
@@ -381,7 +381,7 @@ class FeedbackNode:
                         self.move_front_old(d_x/8) # front in direction of x axis (world coordinate)
                         d_y = tag_pos_y_r -  dist_to_target_y_w  
                         print("DY is: ", d_y)
-                        if math.abs(d_y) > 0.005: # greater than 5cm
+                        if abs(d_y) > 0.005: # greater than 5cm
                             # if the robot is not at zero degrees, then rotate to make it zero
                             # print("Turning to zero degrees...")
                             # self.turn(0,joy_msg)
