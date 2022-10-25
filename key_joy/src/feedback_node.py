89e365691abb8127b4b43f6a4ec4f268dde20065
vicaranq <vdarango@hotmail.com>
2022-10-24 22:52:39 -0700

    Update feedback_node.py

--- key_joy/src/feedback_node.py
+++ key_joy/src/feedback_node.py
@@ -547,8 +547,8 @@ if __name__ == "__main__":
     # print(points)
     # points = [(0,0,0),(1,0,0),(1,1,1.57),(2,1,0),(2,2,-1.57),(1,1,-0.78),(0,0,0)]
 
-    points = [(0.7,0.0,0.0), (0.7,1.4,np.pi), (0.,1.4,0.0), (0.0,0.0,0.0)]    
-    tags = ["marker_1","marker_4","marker_2", "marker_2"] # tag ids associated to each position
+    points = [(0.7,0.0,0.0), (0.7,1.4,np.pi), (0.0,0.0,0.0)]    
+    tags = ["marker_1","marker_4","marker_2"] # tag ids associated to each position
     p, tag_id = (points[0], tags[0])
     '''
     Getting Tag info
