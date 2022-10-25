b167430420bd59aec78901d88134402d11f94940
vicaranq <vdarango@hotmail.com>
2022-10-24 17:01:45 -0700

    Update feedback_node.py

--- key_joy/src/feedback_node.py
+++ key_joy/src/feedback_node.py
@@ -368,6 +368,10 @@ class FeedbackNode:
                     
                     break  
             else:
+
+                self.turn_old(0)
+                time.sleep(1)
+
                 time_per_rad = 2.1/ (math.pi/2)
                 joy_msg = self.get_joy_msg()
                 t_start = time.time()
@@ -375,7 +379,7 @@ class FeedbackNode:
                 joy_msg.axes[THETA] = 1 if rads_to_turn >= 0 else -1# >0.1
                 #while time.time() < t_start + time_per_rad*abs(rads_to_turn):
                 self.pub_joy.publish(joy_msg)
-                time.sleep(2.3)
+                time.sleep(2)
                 #time.sleep(0.1)
                     # just wait for target_time          
                 joy_msg.axes[THETA] = 0 # reset 
