d0bd9d4a2adf575a248ff7b3f41d7bf451b457e0
vicaranq <vdarango@hotmail.com>
2022-10-24 21:46:37 -0700

    Update feedback_node.py

--- key_joy/src/feedback_node.py
+++ key_joy/src/feedback_node.py
@@ -96,7 +96,7 @@ class FeedbackNode:
             self.stop_robot()
 
             #get heuristic angle
-            theta = -1.0*math.asin(tag_pos_y_r/d_x) # must be -1 no? 
+            theta = -1.0*math.asin(tag_pos_y_r/d_x) * 0.7 # must be -1 no? 
             # theta = math.atan(tag_pos_y_r/d_x) # must be -1 no? 
             print("adjusting by: {} deg (tag_pos_y_r: {} and d_x: {})".format(theta*180/math.pi, tag_pos_y_r, d_x))
 
