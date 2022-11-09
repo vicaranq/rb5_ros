correct rotation matrix

key_joy/src/kalman_map.py
@@ -436,10 +436,11 @@ class KalmanNode:
                     self.control_matrix_G = np.zeros((33,1))
 
                 # calculate H
+                # is this the correct rotation matrix
                 transformation_matrix = np.zeros((3,3))
                 transformation_matrix[0][0] = np.cos(self.state[2])
-                transformation_matrix[0][1] = -1.0*np.sin(self.state[2])
-                transformation_matrix[1][0] = np.sin(self.state[2])
+                transformation_matrix[0][1] = 1.0*np.sin(self.state[2])
+                transformation_matrix[1][0] = -1.0*np.sin(self.state[2])
                 transformation_matrix[1][1] = np.cos(self.state[2])
                 transformation_matrix[2][2] = 1
                 for k in range(0,30,3):