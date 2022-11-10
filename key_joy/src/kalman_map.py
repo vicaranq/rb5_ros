62be91b42545bff78ac94ed0be94d2bc4469eb00
vicaranq <vdarango@hotmail.com>
2022-11-09 22:41:55 -0800

    Update kalman_map.py

--- key_joy/src/kalman_map.py
+++ key_joy/src/kalman_map.py
@@ -550,7 +550,7 @@ class KalmanNode:
                 # self.update_G(i)
 
                 # Update robot state, future label states are expected not to move so G is 0 for tags
-                self.state = self.state[:3] + self.control_matrix_G[:3]
+                self.state[:3] = self.state[:3] + self.control_matrix_G[:3]
                 # self.control_matrix_G = np.zeros((33,1))
                 
                 # if we see tags since the last iteration                
