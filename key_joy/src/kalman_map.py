c219151b8f11dffc922581126300114f2d8990c6
vicaranq <vdarango@hotmail.com>
2022-11-10 00:17:58 -0800

    Update kalman_map.py

--- key_joy/src/kalman_map.py
+++ key_joy/src/kalman_map.py
@@ -397,7 +397,7 @@ class KalmanNode:
     def turn_90(self):
         joy_msg = self.get_joy_msg()
         # time_per_rad = 2.3/ (math.pi/2)
-        time_per_rad = 2.8/ (math.pi/2)
+        time_per_rad = 3/ (math.pi/2)
 
         t_start = time.time()
         joy_msg.axes[THETA] = 0.8
