f93c24e31faed150e26f2f7de5b9769c893e6a51
vicaranq <vdarango@hotmail.com>
2022-11-10 19:58:35 -0800

    Update kalman_map.py

--- key_joy/src/kalman_map.py
+++ key_joy/src/kalman_map.py
@@ -429,7 +429,7 @@ class KalmanNode:
         joy_msg = self.get_joy_msg()
         # time_per_rad = 2.3/ (math.pi/2)
         #time_per_rad = 2.8/ (math.pi/2)
-        time_per_rad = 3/ (math.pi/2)
+        time_per_rad = 3.1/ (math.pi/2)
 
         t_start = time.time()
         # joy_msg.axes[THETA] = -0.8
@@ -931,7 +931,7 @@ if __name__ == "__main__":
         print("Starting navigation to target point: ", p)        
         feedback_node.run(p)
     '''
-    for _ in range(1):
+    for _ in range(2):
         kalman_node.run()
     kalman_node.write_saved_data()
     # kalman_node.move_front_new(1)
