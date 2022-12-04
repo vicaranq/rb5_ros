6bf146f8c85f1335a8a6df3e58e7a8ce661e6a53
vicaranq <vdarango@hotmail.com>
2022-12-04 15:43:13 -0800

    Update roomba_node.py

--- key_joy/src/roomba_node.py
+++ key_joy/src/roomba_node.py
@@ -645,6 +645,8 @@ class RoombaNode:
 
         state, target_position_w = next_action
 
+        target_position_w = (target_position_w[0]*0.05, target_position_w[1]*0.05)
+
         print("Navigating from {} --> {}".format((self.x_w,self.y_w, self.theta_w), target_position_w))
         print("--------------------------")
         print("--------------------------")
@@ -815,8 +817,8 @@ if __name__ == "__main__":
     # roomba_node.move_front_no_tag(-1)
 
     # roomba_node.turn_90()
-    roomba_node.turn_90(left=True)
-    roomba_node.turn_90(left=False)
+    # roomba_node.turn_90(left=True)
+    # roomba_node.turn_90(left=False)
 
     # midpoint = mapping_shortest_dist()
     # points = [midpoint[:2], (1.0, 1.0)]
@@ -846,13 +848,13 @@ if __name__ == "__main__":
     #     print("Starting navigation to target point: ", p)               
     #     roomba_node.run(p)
     
-    # test_points = [("F", (37,2)), ("B", (2,2)), ("R", (2,4)),\
-    #                 ("F", (37,4)), ("B", (2,4)), ("R", (2,6)),\
-    #                 ("F", (37,6)), ("B", (2,6)), ("R", (2,8)) 
-    #                 ]
-    # for p in test_points:        
-    #     print("======================================================================")
-    #     print("Starting navigation to target point: ", p)               
-    #     roomba_node.run(p)
+    test_points = [("F", (25,2)), ("B", (2,2)), ("R", (2,4)),\
+                    ("F", (25,4)), ("B", (2,4)), ("R", (2,6)),\
+                    ("F", (25,6)), ("B", (2,6)), ("R", (2,8)) 
+                    ]
+    for p in test_points:        
+        print("======================================================================")
+        print("Starting navigation to target point: ", p)               
+        roomba_node.run(p)
 
-    # roomba_node.run()
+    roomba_node.run()
