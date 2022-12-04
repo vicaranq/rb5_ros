1fc0f6ccc432fd77c9d03c5c45989a124a58c147
vicaranq <vdarango@hotmail.com>
2022-12-04 15:30:19 -0800

    Update roomba_node.py

--- key_joy/src/roomba_node.py
+++ key_joy/src/roomba_node.py
@@ -513,7 +513,7 @@ class RoombaNode:
         joy_msg = self.get_joy_msg()
         # time_per_rad = 2.3/ (math.pi/2)
         #time_per_rad = 2.8/ (math.pi/2)
-        time_per_rad = 2.5/ (math.pi/2)
+        time_per_rad = 2.3/ (math.pi/2)
 
         t_start = time.time()
         
