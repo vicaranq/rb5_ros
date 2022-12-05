6d631c66b26dd4fc29f45edab940178e95d93e63
vicaranq <vdarango@hotmail.com>
2022-12-04 17:18:49 -0800

    Update roomba_node.py

--- key_joy/src/roomba_node.py
+++ key_joy/src/roomba_node.py
@@ -527,7 +527,7 @@ class RoombaNode:
         if left:
             joy_msg.axes[THETA] = -0.85
         else:    
-            joy_msg.axes[THETA] = 0.873
+            joy_msg.axes[THETA] = 0.878
 
         while time.time() < t_start + time_per_rad*np.pi/2:
         # while time.time() < t_start + time_per_rad*angle:
